630fad627e7d0a583776b2072165845338abf580
UriProtocol <angel_3041210085@utd.edu.mx>
2023-03-09 10:00:26 -0600

    nav

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Sidebar from './components/nav/Sidebar'
+
+function App() {
+
+  return (
+    <>
+    <Sidebar />
+    <div>asdasdasdasd</div>
+  </>
+  )
+}
+
+export default App
