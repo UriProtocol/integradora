98ee335713144cad3113a63b97bcacc66c03e764
UriProtocol <angel_3041210085@utd.edu.mx>
2023-04-15 21:59:29 -0600

    agregando animaciones

--- src/components/modals/ModalModificar.jsx
+++ src/components/modals/ModalModificar.jsx
@@ -18,8 +18,8 @@ export default function ModalModificar({ handleSubmit, nombre, isActive, setModa
     return (
     ReactDom.createPortal(
     <>
-        <div className={`bg-black bg-opacity-50 backdrop-blur-sm inset-0 z-[90] fixed grid place-content-center ${isDark ? 'dark' : ''}`} onClick={handleClose}>
-            <div className='bg-white dark:bg-zinc-800 p-8 rounded shadow-lg text-center text-green-900 dark:text-green-50 z-20'>
+        <div className={`pl-8 lg:pl-80 bg-black bg-opacity-50 backdrop-blur-sm inset-0 z-[90] fixed grid place-content-center ${isDark ? 'dark' : ''} animate-fade-in`} onClick={handleClose}>
+            <div className='bg-white dark:bg-zinc-800 p-8 rounded shadow-lg text-center text-green-900 dark:text-green-50 z-20 animate-popup-appear'>
                 <h2 className='text-xl font-semibold pb-2 border-b border-green-700 dark:border-zinc-500'>{nombre}</h2>
                 <p className='mt-4 text-lg'>¿Estás seguro de que deseas realizar estos cambios al paciente?</p>
                 <p className='mt-2 opacity-80'>Los cambios son irreversibles</p>
@@ -28,7 +28,7 @@ export default function ModalModificar({ handleSubmit, nombre, isActive, setModa
                     <button className='btn btn-blue' onClick={handleSubmit}>Aceptar</button>
                 </div>
             </div>
-            <div className='inset-0 fixed z-10' onClick={handleClose}/>
+            <div className='inset-0 fixed z-10' onClick={handleClose}/> {/*Al hacer click en el fondo ta,bién se cerrará el popup*/}
         </div>
     </>,
     document.querySelector('#portal')
