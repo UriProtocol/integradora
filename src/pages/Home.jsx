28c8289cfd8585ea859fa95045ad70052e0eae15
UriProtocol <angel_3041210085@utd.edu.mx>
2023-04-15 22:00:12 -0600

    agregando animaciones

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -80,12 +80,12 @@ const Home = () => {
     
     <div className="grid grid-cols-2  gap-6 gap-x-12 w-full h-full items-center p-8 lg:p-16">
       {/* Mensaje en el lado izquierdo */}
-      <div className="flex flex-col ">
-        <h1 className="text-dark text-4xl sm:text-4xl 2xl:text-5xl ">{mensaje}</h1>
+      <div className="flex flex-col animate-appear-up">
+        <h1 className="text-dark text-4xl sm:text-4xl 2xl:text-5xl">{mensaje}</h1>
       </div>
 
       {/* Botón en el lado derecho */}
-      <div className="flex justify-end">
+      <div className="flex justify-end opacity-0 animate-home-buttons-appear">
         <button onClick={handleAgregar} className="shadow-lg btn btn-green px-6 py-3 rounded-md text-lg sm:px-4 flex items-center gap-1 mr-5">
           <p className="cursor-pointer"><AiOutlinePlus className="inline-block transform -translate-x-1 mb-1" /> Nuevo registro</p>
         </button>
@@ -96,7 +96,7 @@ const Home = () => {
       </div>
 
       {/* PRIMERA CARTA */}
-      <div className="fondo p-8 2xl:p-14 rounded-md shadow flex -mt-10 2xl:-mt-60">
+      <div className="fondo p-8 2xl:p-14 rounded-md shadow flex -mt-10 2xl:-mt-60 opacity-0 animate-card-left-appear">
         {/* Contenido de la primera carta */}
         <AiOutlineTeam className="w-20 h-20 2xl:w-32 2xl:h-32 mr-10 2xl:mr-14" />
         <div className="flex flex-col">
@@ -107,7 +107,7 @@ const Home = () => {
       </div>
 
       {/* SEGUNDA CARTA */}
-      <div className="fondo p-8 2xl:p-14 rounded-md shadow flex -mt-10 2xl:-mt-60">
+      <div className="fondo p-8 2xl:p-14 rounded-md shadow flex -mt-10 2xl:-mt-60 opacity-0 animate-card-right-appear">
         {/* Contenido de la segunda carta */}
         <AiOutlineDiff className="w-20 h-20 2xl:w-28 2xl:h-28 mr-10 2xl:mr-14" />
         <div className="flex flex-col">
@@ -119,7 +119,7 @@ const Home = () => {
       </div>
 
       {/* TERCERA CARTA */}
-      <div className="fondo col-span-2 md:col-span-1 lg:col-span-1 xl:col-span-1 p-5 2xl:p-10 rounded-md shadow -mt-16 2xl:-mt-80" style={{ gridColumn: "span 2 / span 1" }}>
+      <div className="fondo col-span-2 md:col-span-1 lg:col-span-1 xl:col-span-1 p-5 2xl:p-10 rounded-md shadow -mt-16 2xl:-mt-80 opacity-0 animate-card-bottom-appear" style={{ gridColumn: "span 2 / span 1" }}>
         {/* Contenido de la tercera carta */}
         <h1 className="dark:text-gray-500 text-green-600 text-xl sm:text-2xl 2xl:text-3xl mb-5 dark:font-medium">¿Necesitas ayuda?</h1>
         <p className="text-black dark:text-white text-lg sm:text-lg 2xl:text-2xl mb-5">Tenemos para ti la seccion de ayuda, no dudes en usarla si te encuentras atascado en el sistema</p>
