2b712ca582936ad12a91e967912b6fd3b7ce1cd8
UriProtocol <angel_3041210085@utd.edu.mx>
2023-04-16 20:59:33 -0600

    cambio menor

--- src/components/modals/ModalEliminar.jsx
+++ src/components/modals/ModalEliminar.jsx
@@ -21,7 +21,6 @@ export default function ModalEliminar({tipo, handleEliminar, id, nombre, fecha, 
         handleEliminar(id)
         handleClose()
     }
-    
 
     return (
     ReactDom.createPortal(
@@ -35,7 +34,7 @@ export default function ModalEliminar({tipo, handleEliminar, id, nombre, fecha, 
                     <button className='btn btn-red' onClick={() => handleBorrar(id)}>Aceptar</button>
                 </div>
             </div>
-            <div className='inset-0 fixed z-10' onClick={handleClose}/> {/*Al hacer click en el fondo ta,bién se cerrará el popup*/}
+            <div className='inset-0 fixed z-10' onClick={handleClose}/> {/*Al hacer click en el fondo también se cerrará el popup*/}
         </div>
 
     </>,
